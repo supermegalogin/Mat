2e202d2176e4488d2b5bea422e0e311a9e79fc78
supermegalogin <43647354+supermegalogin@users.noreply.github.com>
2019-03-31 12:28:42 +0300

    Update mat.h

--- mat.h
+++ mat.h
@@ -4,8 +4,8 @@ class Mat
 {
 public:
     Mat();
-    void Kv(int pole); // sqv in pole
-    void Simple(long numb); //simple numb
+    void Kv(int pole); // вычисление квадрата числа в поле
+    void Simple(long numb); //разложение числ на простые множества
     void Kr1N1(int numb, int pole); //разрешимость сравнения x^2=numb mod pole
 private: int rez=1;
 };
