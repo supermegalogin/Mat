78f2601cd9f42b65cc867d19efbe7a9a7b51baa8
supermegalogin <43647354+supermegalogin@users.noreply.github.com>
2019-04-02 20:16:28 +0300

    Add files via upload

--- mat.cpp
+++ mat.cpp
@@ -20,6 +20,7 @@ void Mat::Kv(int pole){
 
 int* Mat::Simple(long numb){
     int mas[28]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,71,73,79,83,89,97,101,103,107,109};
+    size=0;
     int* rezMas=new int[size];
     int a, i=0;
     long flag;
@@ -44,15 +45,15 @@ int* Mat::Simple(long numb){
     }
 }
 
-void Mat::symbLegendre(long numb, int pole){
+int Mat::symbLegendre(long numb, int pole){
     try {//проверка на четность
-        if(pole%2==0) throw 0;
+        if((pole%2==0)||(numb==0)) throw 0;
     } catch (int x) {
-        cout<<endl<<"pole должно быть нечетным("<<x<<")";
+        cout<<endl<<"Ошибка ввода("<<x<<")";
         exit(0);
     }
     int mn=1,zam,newPole=pole;
-
+    rez=1;
     if(numb<pole){
         int* rezMas=Simple(numb);
         for (int i=0;i<=size;i++) {
@@ -79,7 +80,7 @@ void Mat::symbLegendre(long numb, int pole){
             }
         }
         cout<<endl<<"Результат сравнения= "<<rez<<endl;
-        exit(0);
+        return rez;
 
     }else{
         zam=pole%numb;
@@ -95,9 +96,9 @@ void Mat::lemmaGausa(int numb, int pole){
     int a;
     bool power=true;
     try {//проверка на четность
-        if(pole%2==0) throw 0;
+        if((pole%2==0)||(numb==0)) throw 0;
     } catch (int x) {
-        cout<<endl<<"pole должно быть нечетным("<<x<<")";
+        cout<<endl<<"Ошибка ввода("<<x<<")";
         exit(0);
     }
     for(int i=1;i<=(pole-1)/2;i++){
@@ -106,4 +107,69 @@ void Mat::lemmaGausa(int numb, int pole){
     }
     if(power){cout<<endl<<"Результат сравнения= "<<1<<endl;}
     else cout<<endl<<"Результат сравнения= "<<-1<<endl;
+}
+
+void Mat::symbJacobi(long numb, int pole){
+    try {
+        if(pole%2==0) throw 10;
+        if(numb==0) {rez=0; throw 0;}
+
+        int zam, newNumb=numb,mn=1;
+        rez=1;
+        if(numb<pole){
+            int* rezMas=Simple(pole);
+            for (int i=0;i<=size;i++) {
+                numb=newNumb;
+
+                for (;;) {
+                    if(numb<rezMas[i])
+                    {
+                        if(numb==0){ rez=0;throw 0;}
+                        if(numb==1) throw 1;
+                        if(numb==2){
+                            zam=(rezMas[i]*rezMas[i]-1)/8;
+                            if(zam%2==1) mn=(-1);
+                            rez*=mn;
+                            break;
+                        }
+                        mn=(numb%4==1)||(rezMas[i]%4==1)? 1:-1;
+                        zam=rezMas[i]%numb;
+                        rezMas[i]=numb;
+                        numb=zam;
+
+                        rez*=mn;
+                    }else {
+                        zam=rezMas[i]%numb;
+                        rezMas[i]=numb;
+
+                        numb=zam;
+                    }
+
+                }
+
+            }
+
+            cout<<endl<<"Результат сравнения= "<<rez<<endl;
+
+        }else {
+            zam=pole%numb;
+            pole=numb;
+            numb=zam;
+            symbJacobi(numb,pole);
+        }
+
+    } catch (int x) {
+        switch (x) {
+        case 10: cout<<endl<<"Ошибка ввода("<<x<<")"<<endl;
+            break;
+        case 0: cout<<endl<<"Результат сравнения= 0"<<endl;
+            break;
+        case 1: cout<<endl<<"Результат сравнения= "<<rez<<endl;
+            break;
+        default: cout<<endl<<"Неизвестная Ошибка ("<<x<<")"<<endl;
+            break;
+        }
+
+    }
+
 }
--- mat.h
+++ mat.h
@@ -6,8 +6,9 @@ public:
     Mat();
     void Kv(int pole); // вычисление квадрата числа в поле
     int* Simple(long numb); //разложение числ на простые множетели
-    void symbLegendre(long numb, int pole); //Символ Лежандра (разрешимость сравнения x^2=numb mod pole)
+    int symbLegendre(long numb, int pole); //Символ Лежандра (разрешимость сравнения x^2=numb mod pole)
     void lemmaGausa(int numb, int pole); //Символ Лежандра по лемме Гаусса
+    void symbJacobi(long numb, int pole); //Символ Якоби (произведение символов Лежандра)
 private: int rez=1, size=0;
 };
 #endif // MAT_H
