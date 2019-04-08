06f073fd4e651954a9dacea7cc89032493412322
Evgeniy <43647354+supermegalogin@users.noreply.github.com>
2019-04-08 12:22:15 +0300

    Add files via upload

--- mat.h
+++ mat.h
@@ -10,7 +10,7 @@ public:
     int symbLegendre2(int numb, int pole);// без разложения на простые множетели => работает со всеми числами
     void lemmaGausa(int numb, int pole); //Символ Лежандра по лемме Гаусса
     void symbJacobi(long numb, int pole); //Символ Якоби (произведение символов Лежандра)
-    void fastPower(long numb, int pole);
+    void fastPower(long numb,int pow, int pole);// возведение целого числа в степень в поле
     void ellCorPoint(int a,int b,int c, int pole); //y^2=ax^3+bx+c
 private: int rez=1, size=0;
 };
