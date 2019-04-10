654c22b07a2e55e41865fed82482c52e5c96e25b
Evgeniy <43647354+supermegalogin@users.noreply.github.com>
2019-04-10 20:13:37 +0300

    Add files via upload

--- mat.cpp
+++ mat.cpp
@@ -51,44 +51,44 @@ int Mat::symbLegendre(long numb, int pole){
     try {//проверка на четность
         if((pole%2==0)||(numb==0)) throw 0;
 
-    int mn=1,zam,newPole=pole;
-    rez=1;
-    if(numb<pole){
-        int* rezMas=Simple(numb);
-        for (int i=0;i<=size;i++) {
+        int mn=1,zam,newPole=pole;
+        rez=1;
+        if(numb<pole){
+            int* rezMas=Simple(numb);
+            for (int i=0;i<=size;i++) {
 
-            int num=rezMas[i],pole=newPole;
-            if(num==-1) throw 1;
-            for (;;) {
-                if(num==1){
-                    break;
-                }
-                if(num==2){
-                    zam=(pole*pole-1)/8;
-                    if(zam%2==1) mn=(-1);
+                int num=rezMas[i],pole=newPole;
+                if(num==-1) throw 1;
+                for (;;) {
+                    if(num==1){
+                        break;
+                    }
+                    if(num==2){
+                        zam=(pole*pole-1)/8;
+                        if(zam%2==1) mn=(-1);
+                        rez*=mn;
+                        break;
+                    }
+
+                    mn=(num%4==1)||(pole%4==1)? 1:-1;
+                    zam=pole%num;
+                    pole=num;
+                    num=zam;
+
                     rez*=mn;
-                    break;
                 }
-
-                mn=(num%4==1)||(pole%4==1)? 1:-1;
-                zam=pole%num;
-                pole=num;
-                num=zam;
-
-                rez*=mn;
             }
+            cout<<endl<<"Результат сравнения= "<<rez<<endl;
+            return rez;
+
+        }else{
+            zam=pole%numb;
+            pole=numb;
+            numb=zam;
+            symbLegendre(numb,pole);
         }
-        cout<<endl<<"Результат сравнения= "<<rez<<endl;
-        return rez;
 
-    }else{
-        zam=pole%numb;
-        pole=numb;
-        numb=zam;
         symbLegendre(numb,pole);
-    }
-
-    symbLegendre(numb,pole);
     } catch (int x) {
         switch (x) {
         case 0: cout<<endl<<"Ошибка ввода("<<x<<")";
@@ -187,7 +187,39 @@ void Mat::symbJacobi(long numb, int pole){
 
 }
 
-//void Mat::fastPower(long numb, int pole){}
+void Mat::fastPower(long numb,int pow, int pole){
+    try {
+        if(numb==0||pole==0) throw 0;
+        if(pow==0) throw 1;
+        int size=0;
+        long ans=numb;
+        int* bitNum=new int[size];
+
+        while (pow!=1) {
+            bitNum[size]=pow%2;
+            size++;
+            pow=pow/2;
+
+        }
+        bitNum[size]=1;
+        size--;
+        for (size;size>=0;size--) {
+            if(bitNum[size]==0) {ans=ans*ans%pole;
+            }else {
+             ans=ans*ans*numb%pole;
+            }
+        }
+        cout<<endl<<"Результат= "<<ans<<endl;
+
+    } catch (int q) {
+        switch (q) {
+        case 0: cout<<endl<<"NOT NULL"<<endl;
+            break;
+        case 1: cout<<endl<<"Результат= 1"<<endl;
+            break;
+        }
+    }
+}
 
 int Mat::symbLegendre2(int numb, int pole){
     int zam,mn=1;
@@ -221,10 +253,10 @@ int Mat::symbLegendre2(int numb, int pole){
             cout<<endl<<"Результат сравнения= "<<rez<<endl;
             return rez;
         }else {
-          zam=pole%numb;
-          pole=numb;
-          numb=zam;
-          symbLegendre2(numb,pole);
+            zam=pole%numb;
+            pole=numb;
+            numb=zam;
+            symbLegendre2(numb,pole);
         }
 
     } catch (int x) {
@@ -241,15 +273,15 @@ int Mat::symbLegendre2(int numb, int pole){
 void Mat::ellCorPoint(int a, int b, int c, int pole){
 
     try {
-        if(a==0&&b==0&&pole==0)throw 0;
-       int Y[pole/2], X[pole];
-       for (int i=0;i<=pole/2;i++)  Y[i]=i*i%pole;
-      for (int i=0;i<pole;i++)  X[i]=(a*i*i*i+b*i+c)%pole;
-       for (int i=0;i<pole;i++) {
-           for (int j=0;j<=pole/2;j++) {
-               if(X[i]==Y[j]) cout<<endl<<"( "<<i<<"; ±"<<j<<")";
-           }
-       }
+        if(a==0||b==0||pole==0)throw 0;
+        int Y[pole/2], X[pole];
+        for (int i=0;i<=pole/2;i++)  Y[i]=i*i%pole;
+        for (int i=0;i<pole;i++)  X[i]=(a*i*i*i+b*i+c)%pole;
+        for (int i=0;i<pole;i++) {
+            for (int j=0;j<=pole/2;j++) {
+                if(X[i]==Y[j]) cout<<endl<<"( "<<i<<"; ±"<<j<<")";
+            }
+        }
 
 
     } catch (int q) {
--- mat.h
+++ mat.h
@@ -10,8 +10,8 @@ public:
     int symbLegendre2(int numb, int pole);// без разложения на простые множетели => работает со всеми числами
     void lemmaGausa(int numb, int pole); //Символ Лежандра по лемме Гаусса
     void symbJacobi(long numb, int pole); //Символ Якоби (произведение символов Лежандра)
-    void fastPower(long numb, int pole);
-    void ellCorPoint(int a,int b,int c, int pole); //y^2=ax^3+bx+c
+    void fastPower(long numb,int pow, int pole);// возведение целого числа в степень в поле
+    void ellCorPoint(int a,int b,int c, int pole); //y^2=ax^3+bx+c нахождение точек эллиптической кривой
 private: int rez=1, size=0;
 };
 #endif // MAT_H
